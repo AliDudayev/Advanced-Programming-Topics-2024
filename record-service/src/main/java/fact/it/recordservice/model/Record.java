dbf4e5717d592edb6a4dec75d6858672b9971f9b
AliDudayev <r0790996@student.thomasmore.be>
2024-10-12 14:34:59 +0200

    Record service change

--- record-service/src/main/java/fact/it/recordservice/model/Record.java
+++ record-service/src/main/java/fact/it/recordservice/model/Record.java
@@ -1,21 +1,25 @@
-package fact.it.recordservice.dto;
+package fact.it.recordservice.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.time.Duration;
 
-@Data
-@Builder
+@Document(value = "record")
 @AllArgsConstructor
 @NoArgsConstructor
-public class RecordRequest {
+@Builder
+@Data
+public class Record {
+    private String id;
     private Double fastestTime; // optional
     private Double longestDistance; // optional
     private Double maxWeightLifted; // optional
     private Double longestWorkoutDuration; // optional
     private Double mostCaloriesBurned; // optional
+
 }
