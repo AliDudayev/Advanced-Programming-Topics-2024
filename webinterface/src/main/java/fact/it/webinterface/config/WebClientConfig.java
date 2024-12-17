f7436a23bb4951d16b183b5111bd070c4a9ec8b8
Marnick Michielsen <113896267+MarnickM@users.noreply.github.com>
2024-12-17 16:44:31 +0100

    changed user

--- webinterface/src/main/java/fact/it/webinterface/config/WebClientConfig.java
+++ webinterface/src/main/java/fact/it/webinterface/config/WebClientConfig.java
@@ -2,14 +2,14 @@ package fact.it.webinterface.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(){
-//        System.out.println("Creating WebClient bean");
-        return WebClient.builder().build();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
